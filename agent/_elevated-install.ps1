f6ee5b20f78eb2aa12203067168d52ad4b72c42c
System Admin <sysadmin@labnet.local>
2026-06-29 16:59:58 +0300

    chore: remove temporary installer artifacts from repo

--- agent/_elevated-install.ps1
+++
@@ -1,4 +0,0 @@
-﻿Write-Host "Elevated install starting..."
-& "C:\Users\sysadmin\Downloads\ad-manager\agent\install-product.ps1" -ServerUrl "http://localhost:3000" -Silent
-Start-Sleep -Seconds 3
-Get-ScheduledTask | Where-Object { $_.TaskName -like '*ADManager*' } | Select-Object TaskName, State, @{Name='UserId';Expression={$_.Principal.UserId}} | Format-Table -AutoSize
